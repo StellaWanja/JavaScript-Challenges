40781843fcde9a649b428a57a15dc5be259d4a5d
StellaWanja <stylades@gmail.com>
2020-11-04 18:21:50 +0300

    second challenge

--- index.js
+++ index.js
@@ -3,3 +3,27 @@
 function createPhoneNumber(number) {
     return `(${number.slice(0, 3).join('')}) ${number.slice(3, 6).join('')}-${number.slice(6).join('')}`;
 }
+
+// Given an array of integers, find the one that appears an odd number of times.
+//There will always be only one integer that appears an odd number of times.
+function findOdd(A) {
+    //store info about how many times each integer appears
+    let numOfCounts = {};  
+    
+    //count number of times integer appears in array
+    for(let i=0; i<A.length;i++){
+      let num = A[i];
+      numOfCounts[num] = numOfCounts[num] ? numOfCounts[num]+1:1;
+    }
+    let n;
+    let countInt;
+    
+    for(n in numOfCounts){
+      if(numOfCounts[n] % 2 !== 0){
+        countInt = numOfCounts[n];
+      }
+    }
+    let valInString = Object.keys(numOfCounts).find(key => numOfCounts[key] === countInt);
+    
+    return parseInt(valInString);
+  }
