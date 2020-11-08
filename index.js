7a0d45eb10b143f635255336db365ba2768ac123
StellaWanja <stylades@gmail.com>
2020-11-08 23:02:53 +0300

    challenge3

--- index.js
+++ index.js
@@ -27,3 +27,9 @@ function findOdd(A) {
     
     return parseInt(valInString);
   }
+
+ // Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+ function XO(str) {
+  return str.toLowerCase().split('x').length === str.toLowerCase().split('o').length;
+}
